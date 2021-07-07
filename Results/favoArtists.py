ae0909abb051bc3914fa08c1258458364ea7e3d5
Sarvesh MD <mdsarvesh1013@gmail.com>
2021-07-07 16:40:52 +0530

    Copied favoSongs.py to favoArtists.py Edited favoArtists.py so that it would create summary based on the artist of the track. Cleaned up favoSongs.py, renamed sqlite table, removed json comments

--- Results/favoArtists.py
+++ Results/favoArtists.py
@@ -9,8 +9,8 @@ songsArtistsCursor = songsArtistsConn.cursor()
 
 summary = {}
 cursor.execute('''
-CREATE TABLE IF NOT EXISTS songSummary (
-    trackId INTEGER UNIQUE,
+CREATE TABLE IF NOT EXISTS artistSummary (
+    artistId INTEGER UNIQUE,
     totalMsPlayed INTEGER
     )
 ''')
@@ -21,29 +21,29 @@ for item in streamingHistory:
     trackId = item[1]
     artistId = item[2]
     msPlayed = item[3]
-    if trackId not in summary.keys():
-        summary[trackId] =  msPlayed
+    if artistId not in summary.keys():
+        summary[artistId] =  msPlayed
     else :
-        summary[trackId] += msPlayed
+        summary[artistId] += msPlayed
 
-for trackId, totalMsPlayed in summary.items():
-    cursor.execute("INSERT OR IGNORE INTO songSummary (trackId, totalMsPlayed) VALUES (?, ?)", (trackId, totalMsPlayed))
+for artistId, totalMsPlayed in summary.items():
+    cursor.execute("INSERT OR IGNORE INTO artistSummary (artistId, totalMsPlayed) VALUES (?, ?)", (artistId, totalMsPlayed))
 connection.commit()
 
-cursor.execute("SELECT * FROM songSummary ORDER BY totalMsPlayed DESC")
+cursor.execute("SELECT * FROM artistSummary ORDER BY totalMsPlayed DESC")
 summaryFromTable = cursor.fetchall()
 
 i = 0
 table = list()
-for trackId, totalMsPlayed in summaryFromTable:
+for artistId, totalMsPlayed in summaryFromTable:
     i += 1
-    songsArtistsCursor.execute(f'SELECT trackName FROM Songs WHERE id IS {trackId}')
-    trackName = songsArtistsCursor.fetchone()[0]
+    songsArtistsCursor.execute(f'SELECT artistName FROM Artists WHERE id IS {artistId}')
+    artistName = songsArtistsCursor.fetchone()[0]
     convertedPlayed = str(timedelta(milliseconds=totalMsPlayed)).split(":")
     hours = int(convertedPlayed[0])
     minutes = int(convertedPlayed[1])
     seconds = float(convertedPlayed[2])
-    table.append([i, trackName, hours, minutes, f"{seconds:.2f}"])
-table = tabulate(table, headers=['S.No', 'Track Name', 'Hours', 'Minutes', 'Seconds'], tablefmt="pipe")
+    table.append([i, artistName, hours, minutes, f"{seconds:.2f}"])
+table = tabulate(table, headers=['S.No', 'Artist Name', 'Hours', 'Minutes', 'Seconds'], tablefmt="pipe")
 print()
 print(table)
